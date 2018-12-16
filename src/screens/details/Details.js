3bd820edf059ce4cf9a8028520203826b543516a
pwndubey77 <pwndubey77@gmail.com>
2018-12-16 23:15:56 +0530

    Details page integrated

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import logo from '../../assets/FastFood.svg';
 import "./Details.css";
 
-import mock from "../../assets/mock.jpg";
+//import mock from "../../assets/mock.jpg";
 
-import Header from '../../common/header/header';
+import Header from '../../common/header/Header';
 import { List, ListItemText, ListItemSecondaryAction, Card, CardContent, Badge } from '@material-ui/core';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
@@ -25,7 +25,7 @@ export default class Details extends Component {
         {/* Restaurant info */}
         <div className = "restaurant-info">
           <div className = "restaurant-img">
-            <img src = {mock} />
+            {/* <img src = {mock} /> */}
           </div>
 
           <div className = "restaurant-text-info">
@@ -33,7 +33,7 @@ export default class Details extends Component {
             <div className = "restaurant-locality">cbd-belapur</div>
             <div className = "restaurant-categories">Chinese, Continental, Indian, Italian</div>
             <div className = "restaurant-info-footer">
-              <div className = "restaurant-rating"><i class="fa fa-star" aria-hidden="true"></i> 4.4
+              <div className = "restaurant-rating"><i className="fa fa-star" aria-hidden="true"></i> 4.4
                 <br />
                 <span className = "restaurant-rating-text">
                   AVERAGE RATING BY
@@ -63,10 +63,10 @@ export default class Details extends Component {
               <Divider />
               <List component = "div">
                 <ListItem>
-                  <i style = {{color: "green"}} class="fa fa-circle" aria-hidden="true"></i>
+                  <i style = {{color: "green"}} className="fa fa-circle" aria-hidden="true"></i>
                   <ListItemText primary = "hakka noodles" />
                   <ListItemSecondaryAction>
-                    <i class="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
+                    <i className="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
                     <IconButton aria-label = "Add">
                       <Add />
                     </IconButton>
@@ -74,10 +74,10 @@ export default class Details extends Component {
                 </ListItem>
 
                 <ListItem>
-                  <i style = {{color: "red"}} class="fa fa-circle" aria-hidden="true"></i>
+                  <i style = {{color: "red"}} className="fa fa-circle" aria-hidden="true"></i>
                   <ListItemText primary = "hakka noodles" />
                   <ListItemSecondaryAction>
-                    <i class="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
+                    <i className="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
                     <IconButton aria-label = "Add">
                       <Add />
                     </IconButton>
@@ -91,10 +91,10 @@ export default class Details extends Component {
               <Divider />
               <List component = "div">
                 <ListItem>
-                  <i style = {{color: "red"}} class="fa fa-circle" aria-hidden="true"></i>
+                  <i style = {{color: "red"}} className="fa fa-circle" aria-hidden="true"></i>
                   <ListItemText primary = "hakka noodles" />
                   <ListItemSecondaryAction>
-                    <i class="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
+                    <i className="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
                     <IconButton aria-label = "Add">
                       <Add />
                     </IconButton>
@@ -102,10 +102,10 @@ export default class Details extends Component {
                 </ListItem>
 
                 <ListItem>
-                  <i style = {{color: "red"}} class="fa fa-circle" aria-hidden="true"></i>
+                  <i style = {{color: "red"}} className="fa fa-circle" aria-hidden="true"></i>
                   <ListItemText primary = "hakka noodles" />
                   <ListItemSecondaryAction>
-                    <i class="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
+                    <i className="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
                     <IconButton aria-label = "Add">
                       <Add />
                     </IconButton>
@@ -126,13 +126,13 @@ export default class Details extends Component {
 
               <List component = "div">
                 <ListItem>
-                  <i style = {{color: "red"}} class="fa fa-stop-circle-o" aria-hidden="true"></i>
+                  <i style = {{color: "red"}} className="fa fa-stop-circle-o" aria-hidden="true"></i>
                   <ListItemText primary = "hakka noodles" />
                   <ListItemSecondaryAction>
                     <IconButton aria-label = "Add">
                       <Add />
                     </IconButton>
-                    <i class="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
+                    <i className="fa fa-inr" aria-hidden="true"></i> {mockPrice.toFixed(2)}
                   </ListItemSecondaryAction>
                 </ListItem>
               </List>
