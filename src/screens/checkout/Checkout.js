44d2f470d0256083c496b338e02a17ee0602724c
pwndubey77 <pwndubey77@gmail.com>
2018-12-20 18:11:41 +0530

    removed unused imports and comments

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -1,27 +1,9 @@
 import React, { Component } from 'react';
-import logo from '../../assets/FastFood.svg';
 import { withStyles } from '@material-ui/core/styles';
-//import PropTypes from "prop-types";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { white } from 'material-ui/styles/colors';
 import CheckoutPage from "./VerticalStepper";
 import "./Checkout.css";
 import Header from '../../common/header/Header'
-
-
-
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
-// import Divider from '@material-ui/core/Divider';
-// import Snackbar from '@material-ui/core/Snackbar';
-// import IconButton from '@material-ui/core/IconButton';
-// import CloseIcon from '@material-ui/icons/Close';
-// import 'font-awesome/css/font-awesome.min.css';
-// import Typography from '@material-ui/core/Typography';
-
-
-
 
     const styles = {
         root: {
